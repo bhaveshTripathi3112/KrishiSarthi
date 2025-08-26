6728701aaaa8faab5b5cee2aa1695b5689fd505d
bhavesh <bhaveshtripathi3112@gmail.com>
2025-08-26 14:47:48 +0530

    authentication-backend-added

---
+++ backend/config/connectDB.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)   
+        console.log("DB connected");
+        
+    } catch (error) {
+        console.log("DB error : ",error);
+        
+    }
+
+
+}
